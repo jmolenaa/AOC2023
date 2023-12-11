7e91c6df10e799626ad77c679fe6120297f1890e
jmolenaa <jmolenaa@student.codam.nl>
2023-12-11 09:03:23 +0100

    cleaned day10

--- day10/python/sol.py
+++ day10/python/sol.py
@@ -5,9 +5,11 @@ def lookup_direction(val, dir1):
 
 def find_start(lines):
 	for i, line in enumerate(lines):
-		if line.find("S") != -1:
+		if "S" in line:
 			return ([i, line.find("S")])
 
+# we look around the start position and find the pipes that connect to the start
+# we have extra checks so we don't go out of bounds
 def identify_pipe_type(start_position, lines, pipes):
 	dir = list()
 	if start_position[0] > 0:
@@ -36,103 +38,72 @@ def change_pos(cur_pos, cur_pipe, prev_dir):
 		return ([cur_pos[0] - 1, cur_pos[1]])
 	elif new_direction == "down":
 		return ([cur_pos[0] + 1, cur_pos[1]])
-	
+
 def opposite(prev_dir):
-	if prev_dir == "up":
-		return "down"
-	elif prev_dir == "down":
-		return "up"
-	elif prev_dir == "left":
-		return "right"
-	elif prev_dir == "right":
-		return "left"
+	directions = {"up": "down", "down": "up", "left": "right", "right": "left"}
+	return directions[prev_dir]
 
 def main():
-	lines = open("input").readlines()
-	for i, line in enumerate(lines):
-		lines[i] = line.strip()
+	lines = [line.strip() for line in open("input").readlines()]
 
+	# finds coordinates of start position
 	start_position = find_start(lines)
+
+	# dictionary of pipes with the directions they face as values
 	global pipes
 	pipes = {"|": ["up", "down"], "-": ["left", "right"], "F": ["down", "right"], \
 		  	"J": ["up", "left"], "L": ["up", "right"], "7": ["left", "down"]}
 
+	# make empty grid where we can put our map with just the loop pipes
+	grid = [['.'] * len(line) for line in lines]
 
-
-	
-	# print(lookup_dict("|", "up", pipes))
-	# for key, value in pipes.items():
-		# print(key, value)
-		
-	# huh = pipe("|", "up", "down")
-	# print(huh.outgoing_dir("up"))
-
-	# print(huh.dir1, huh.dir2, huh.val)
-
-	# grid = [["."] * len(lines[0])] * len(lines)
-	# for line in grid:
-	# 	print(line)
-	# print(grid)
-	grid = list()
-	for line in lines:
-		grid.append(['.'] * len(line))
-	# for line in grid:
-	# 	print(line)
-	# grid[0][0] = 'X'
-	# for line in grid:
-	# 	print(line)
-		# grid.append(list(line))
-	# print(grid)
-	# print(list(lines[0]))
-	start_pipe = identify_pipe_type(start_position, lines, pipes)
-	current_pipe = start_pipe
+	# identifies the pipe we start with and sets up starting values for loop
+	current_pipe = identify_pipe_type(start_position, lines, pipes)
 	current_pos = start_position
-	previous_dir = pipes[start_pipe][0]
-	print(current_pos, current_pipe, previous_dir)
+	previous_dir = pipes[current_pipe][0]
 	steps = 0
-	area = 0
+
+
 	while True:
-		# if steps == 10:
-		# 	break
 
-
+		# making grid with only loop pipes
 		grid[current_pos[0]][current_pos[1]] = current_pipe
-		previous_pos = current_pos
 		current_pos = change_pos(current_pos, current_pipe, previous_dir)
-		previous_dir = lookup_direction(current_pipe, previous_dir)
-		previous_dir = opposite(previous_dir)
+
+		# we look at the direction we left the pipe in and grav the opposite for the next loop
+		previous_dir = opposite(lookup_direction(current_pipe, previous_dir))
 		current_pipe = lines[current_pos[0]][current_pos[1]]
-		# print(current_pos, current_pipe, previous_dir)
-		# print("NEXT")
 		steps += 1
 
-		area += current_pos[1] * previous_pos[0] - current_pos[0] * previous_pos[1] 
-
+		# if we reach start again we finished the pipe loop
 		if current_pos == start_position:
 			break
-		# print("lol")
-		# break
-	print(steps / 2)
+
+
+	# changes our lists inside grid ot strings for faster access
 	for i, line in enumerate(grid):
 		grid[i] = ''.join(line)
-	for line in grid:
-		print(line)
-	counter = 0
+		
+	# for line in grid:
+	# 	print(line)
+
+
+	# loop through the grid and find all inside points
+	# if we encounter a pipe that goes up that means we are now inside the loop
+	# if we encounter another one we are outside etc.
+	# we only count points when we are inside
+	inside_points = 0
 	for line in grid:
 		inside = 0
 		for char in line:
 			if char != "." and pipes[char][0] == "up":
 				inside = (inside + 1) % 2
 			elif char == "." and inside == 1:
-				counter += 1
-	print(counter)
-	# print(grid)
-	# print(start_position)
+				inside_points += 1
 
 
-
-	resultpart1 = 0
-	resultpart2 = 0
+	print(f"The answer to part 1 is: {int(steps / 2)}")
+	print(f"The answer ot part 2 is: {inside_points}")
 
 if __name__ == "__main__":
 	main()
